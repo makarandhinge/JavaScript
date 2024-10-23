05f8ccff1d661916a6c3e7b881a015497324fd2f
Makarand Hinge <mmh2212002@gmail.com>
2024-10-23 18:12:31 +0530

    New folder created

--- otp.js
+++
@@ -1,9 +0,0 @@
-let otpgenerator = (digit) => {
-    let num = '0123456789'
-    let otp = ''
-    for(let i=1; i<=digit ; i++){
-        otp += num[Math.floor(Math.random()*10)]
-    }
-    return otp
-}
-console.log(otpgenerator(6))
